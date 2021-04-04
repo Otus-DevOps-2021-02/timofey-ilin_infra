cb60ea270d893f172773cd1f01d1680834f15005
timofey-ilin <tilin@diasoft.ru>
2021-04-04 15:52:17 +0300

    HW terraform-2

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,12 @@
+variable subnet_id {
+  # Описание переменной
+  description = "net"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "key_pub"
+}
+variable app_disk_image {
+  description = "fd83ldnc1mn7auk6nfii"
+  default     = "reddit-base-app"
+}
