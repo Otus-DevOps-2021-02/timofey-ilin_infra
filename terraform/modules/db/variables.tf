d498bd89fdb4f1b1422bf7ea13ddcfd5ac3242b4
timofey-ilin <tilin@diasoft.ru>
2021-04-04 15:52:17 +0300

    HW terraform-2

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,12 @@
+variable subnet_id {
+  # Описание переменной
+  description = "net"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "key_pub"
+}
+variable db_disk_image {
+  description = "fd8bdlvcehd1e1hq5qcs"
+  default     = "reddit-base-db"
+}
